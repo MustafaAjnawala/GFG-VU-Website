85d287061942a0037c24d5b82c9c6485ad5eb8d4
MustafaAjnawala <117845490+MustafaAjnawala@users.noreply.github.com>
2024-04-22 17:58:42 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GFG Student Chapter | VU</title>
+
+    <!-- important Meta tag and SEO -->
+    <meta name="description" content="GeeksforGeeks Student Chapter of Vishwakarma University.A community where developers, designers and programming enthusiast unite to learn, innovate, and thrive together">
+    <meta name="keyoards" content="GFG, gfg, GeeksforGeeks, student Chapter, Vishwakarma University">
+    <meta itemprop="description" content="GeeksforGeeks Student Chapter of Vishwakarma University.A community where developers, designers and programming enthusiast unite to learn, innovate, and thrive together">
+    <meta name="twitter:title" content="GeeksforGeeks Student Chapter | Vishwakarma University">
+    <meta name="twitter:description" content="GeeksforGeeks Student Chapter of Vishwakarma University.A community where developers, designers and programming enthusiast unite to learn, innovate, and thrive together">
+
+    <!-- By preconnecting to the host, the browser can set up the necessary sockets ahead of time, eliminating extra round trips and saving time -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <!-- Google Fonts | Roboto -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+    <!-- icons library -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet"/>
+
+    <!-- carousel library -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+    <!-- animation on scroll library -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <!-- Linking css file -->
+    <link rel="stylesheet" href="CSS/about.css">
+    <link rel="stylesheet" href="CSS/contact.css">
+    <link rel="stylesheet" href="CSS/design.css">
+    <link rel="stylesheet" href="CSS/events.css">
+    <link rel="stylesheet" href="CSS/footer.css">
+    <link rel="stylesheet" href="CSS/general.css">
+    <link rel="stylesheet" href="CSS/navbar.css">
+    <link rel="stylesheet" href="CSS/teams.css">
+</head>
+<body>
+    <header></header>
+    <main>
+        <section class="container">
+
+            <!-- Sahil's section -->
+            <div class="about-us"></div>
+
+            <!-- Mustafa's section -->
+            <div class="events"></div>
+
+            <!-- Hardik's section -->
+            <div class="teams">
+                <h1 id="heading"><span>Core</span>Team Members</h1>
+                <div class="teams-container">
+                    <div class="card">
+                        <div class="photo">
+                            <img src="images/chairperson.jpg">
+                        </div>
+                        <div class="c-content">
+                            <h3 class="name">Ajab Raja</h3>
+                            <p class="title">Chairperson</p>
+                        </div>
+                        <ul class="social-links">
+                            <li><a href="https://www.linkedin.com/in/ajab-raja-315444225?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BZwH3c39ZRO6ZsFKmw8SkjA%3D%3D" target="_blank" aria-hidden="true" class='bx bxl-linkedin-square'></a></li>
+                            <li><a href="https://github.com/ajeebz" target="_blank" aria-hidden="true" class='bx bxl-github'></a></li>
+                            <li><a href="https://www.instagram.com/ajabb_sii/" target="_blank" aria-hidden="true" class='bx bxl-instagram'></a></li>
+                            <li><a href="https://x.com/ajabb_sii" target="_blank" aria-hidden="true" class='bx bxl-twitter'></a></li>
+                        </ul>
+                    </div>
+
+                    <div class="card">
+                        <div class="photo">
+                            <img src="images/vice-chairperson.jpg">
+                        </div>
+                        <div class="c-content">
+                            <h3 class="name">Krish Gupta</h3>
+                            <p class="title">Vice-Chairperson</p>
+                        </div>
+                        <ul class="social-links">
+                            <li><a href="https://www.linkedin.com/in/krish-gupta-?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B7cQpWI4AQuG8gdqHEJ5jaA%3D%3D" target="_blank" aria-hidden="true" class='bx bxl-linkedin-square'></a></li>
+                            <li><a href="https://github.com/codingwithkrish" target="_blank" aria-hidden="true" class='bx bxl-github'></a></li>
+                            <li><a href="https://www.instagram.com/codewithkrish/" target="_blank" aria-hidden="true" class='bx bxl-instagram'></a></li>
+                            <li><a href="https://x.com/_krishgupta" target="_blank" aria-hidden="true" class='bx bxl-twitter'></a></li>
+                        </ul>
+                    </div>
+
+                    <div class="card">
+                        <div class="photo">
+                            <img src="images/tech-lead.jpg">
+                        </div>
+                        <div class="c-content">
+                            <h3 class="name">Aryan Mehta</h3>
+                            <p class="title">Tech Lead</p>
+                        </div>
+                        <ul class="social-links">
+                            <li><a href="https://www.linkedin.com/in/aryan-mehta230723?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2BVocFEpdTgWv8B2PShg11w%3D%3D" target="_blank" aria-hidden="true" class='bx bxl-linkedin-square'></a></li>
+                            <li><a href="https://github.com/AryanMehta-xd" target="_blank" aria-hidden="true" class='bx bxl-github'></a></li>
+                            <li><a href="https://www.instagram.com/aryanmehta_xd/" target="_blank" aria-hidden="true" class='bx bxl-instagram'></a></li>
+                        </ul>
+                    </div>
+
+                    <div class="card">
+                        <div class="photo">
+                            <img src="images/marketing-head.jpg">
+                        </div>
+                        <div class="c-content">
+                            <h3 class="name">Jay Khabde</h3>
+                            <p class="title">Marketing Head</p>
+                        </div>
+                        <ul class="social-links">
+                            <li><a href="https://www.linkedin.com/in/jay-khabde-914059236?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2FQ4or%2BRLSByfwJDCCDtooA%3D%3D" target="_blank" aria-hidden="true" class='bx bxl-linkedin-square'></a></li>
+                            <li><a href="https://github.com/Jaykhabde" target="_blank" aria-hidden="true" class='bx bxl-github'></a></li>
+                            <li><a href="https://www.instagram.com/jaykhabde/" target="_blank" aria-hidden="true" class='bx bxl-instagram'></a></li>
+                            <li><a href="https://x.com/jaykhabde" target="_blank" aria-hidden="true" class='bx bxl-twitter'></a></li>
+                        </ul>
+                    </div>
+
+                    <div class="card">
+                        <div class="photo">
+                            <img src="images/design-head.jpg">
+                        </div>
+                        <div class="c-content">
+                            <h3 class="name">Yojana Naik</h3>
+                            <p class="title">Design & Branding Head</p>
+                        </div>
+                        <ul class="social-links">
+                            <li><a href="https://www.linkedin.com/in/yojananaik25?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B2SwhBvWoRE6cqnsU7ChSZw%3D%3D" target="_blank" aria-hidden="true" class='bx bxl-linkedin-square'></a></li>
+                            <li><a href="https://github.com/yojananaik" target="_blank" aria-hidden="true" class='bx bxl-github'></a></li>
+                            <li><a href="https://www.instagram.com/yojananaik/" target="_blank" aria-hidden="true" class='bx bxl-instagram'></a></li>
+                            <li><a href="https://x.com/yojananaik_25" target="_blank" aria-hidden="true" class='bx bxl-twitter'></a></li>
+                        </ul>
+                    </div>
+
+                    <div class="card">
+                        <div class="photo">
+                            <img src="images/social-meadia-head.jpg">
+                        </div>
+                        <div class="c-content">
+                            <h3 class="name">Aarohi Samdani</h3>
+                            <p class="title">Social Media Head</p>
+                        </div>
+                        <ul class="social-links">
+                            <li><a href="https://www.linkedin.com/in/aarohi-samdani-b494b0244?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BMCgMMG6fTvuwsWYoxKbTVQ%3D%3D" target="_blank" aria-hidden="true" class='bx bxl-linkedin-square'></a></li>
+                            <li><a href="https://github.com/Aarohi235" target="_blank" aria-hidden="true" class='bx bxl-github'></a></li>
+                            <li><a href="https://www.instagram.com/aarohi.s/" target="_blank" aria-hidden="true" class='bx bxl-instagram'></a></li>
+                        </ul>
+                    </div>
+
+                    <div class="card">
+                        <div class="photo">
+                            <img src="images/pr-head.jpg">
+                        </div>
+                        <div class="c-content">
+                            <h3 class="name">Tejas Jadhav</h3>
+                            <p class="title">PR & Outreach Head</p>
+                        </div>
+                        <ul class="social-links">
+                            <li><a href="https://www.linkedin.com/in/tejas-jadhav-884704152?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bxk2oFiXDSQufjfo9A9Ib0A%3D%3D" target="_blank" aria-hidden="true" class='bx bxl-linkedin-square'></a></li>
+                            <li><a href="https://github.com/tejasjadhav128" target="_blank" aria-hidden="true" class='bx bxl-github'></a></li>
+                            <li><a href="https://www.instagram.com/tejas_0303/" target="_blank" aria-hidden="true" class='bx bxl-instagram'></a></li>
+                        </ul>
+                    </div>
+
+                    <div class="card">
+                        <div class="photo">
+                            <img src="images/event-head.jpg">
+                        </div>
+                        <div class="c-content">
+                            <h3 class="name">Rashi Chincholkar</h3>
+                            <p class="title">Events Head</p>
+                        </div>
+                        <ul class="social-links">
+                            <li><a href="https://www.linkedin.com/in/rashi-chincholkar-15185b269?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BLDoYCEeXQ4iIyRQ2fPoMzQ%3D%3D" target="_blank" aria-hidden="true" class='bx bxl-linkedin-square'></a></li>
+                            <li><a href="https://github.com/Rashi2106-Chincholkar" target="_blank" aria-hidden="true" class='bx bxl-github'></a></li>
+                            <li><a href="https://www.instagram.com/i_.rashi_/" target="_blank" aria-hidden="true" class='bx bxl-instagram'></a></li>
+                            <li><a href="https://x.com/Rashi2106" target="_blank" aria-hidden="true" class='bx bxl-twitter'></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Abhi's section -->
+            <div class="social-media"></div>
+
+            <!-- Affan's section -->
+            <div class="contact-us"></div>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScFdsqM_x0gEh3pFOqbG7DYzI-WyBdIOKBM64zdvGjPeZC55A/viewform?embedded=true" width="640" height="950" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        </section>
+    </main>
+    <footer></footer>
+
+    <!-- initialize the animation library -->
+    <script>AOS.init();</script>
+</body>
+</html>
