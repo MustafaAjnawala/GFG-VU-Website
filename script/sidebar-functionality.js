0a77e0fd9bfcc7adce7b8cb86a553e1e767fb3db
Hardik <hardik.chawhan20@gmail.com>
2024-06-03 00:57:59 +0530

    fixed issue no #5

--- script/sidebar-functionality.js
+++ script/sidebar-functionality.js
@@ -4,6 +4,12 @@ document.addEventListener('click', function(event) {
     
     // Check if the viewport width is below a certain threshold or it's a mobile device
     if (window.innerWidth <= 801 || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        // Check if the clicked element is a link
+        if (event.target.closest('a')) {
+            return; // If it's a link, do nothing and return early
+        }
+        
+        // Check if the click is outside the sidebar and menu button
         if (!sidebar.contains(event.target) && !menuButton.contains(event.target)) {
             hideSidebar();
         }
